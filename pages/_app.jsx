88d684227edd32850d5022aa7aa292e140d20da5
Tretiak Ihor <tretiak.ihor.work@gmail.com>
2022-07-12 19:51:57 +0300

    chore: Styles
    Add Material UI as a library of React UI components

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,24 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
+import { CacheProvider } from '@emotion/react'
+
+import { createEmotionCache } from '../styles/material-ui/createEmotionCache'
+import { theme } from '../styles/material-ui/theme'
+
+const clientSideEmotionCache = createEmotionCache()
+
+const MyApp = ({ Component, pageProps, emotionCache = clientSideEmotionCache }) => (
+	<CacheProvider value={emotionCache}>
+		<Head>
+			<meta name="viewport" content="initial-scale=1, width=device-width" />
+		</Head>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	</CacheProvider>
+)
 
 export default MyApp
---
+++ pages/_document.jsx
@@ -0,0 +1,68 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+import createEmotionServer from '@emotion/server/create-instance'
+
+import { createEmotionCache } from '../styles/material-ui/createEmotionCache'
+import { theme } from '../styles/material-ui/theme'
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					{/* PWA primary color */}
+					<meta name="theme-color" content={theme.palette.primary.main} />
+					<link rel="shortcut icon" href="/favicon/favicon.ico" />
+					<link
+						rel="preload"
+						as="font"
+						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+						type="font/woff2"
+						crossOrigin="true"
+					/>
+					{/* Inject MUI styles first to match with to prepend: true configuration. */}
+					{this.props.emotionStyleTags}
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+	const originalRenderPage = ctx.renderPage
+
+	// You can consider sharing the same Emotion cache between all the SSR requests to speed up performance.
+	// However, be aware that it can have global side effects.
+	const cache = createEmotionCache()
+	const { extractCriticalToChunks } = createEmotionServer(cache)
+
+	ctx.renderPage = () =>
+		originalRenderPage({
+			enhanceApp: (App) =>
+				function EnhanceApp(props) {
+					return <App emotionCache={cache} {...props} />
+				},
+		})
+
+	const initialProps = await Document.getInitialProps(ctx)
+	// This is important. It prevents Emotion to render invalid HTML.
+	const emotionStyles = extractCriticalToChunks(initialProps.html)
+	const emotionStyleTags = emotionStyles.styles.map((style) => (
+		<style
+			data-emotion={`${style.key} ${style.ids.join(' ')}`}
+			key={style.key}
+			dangerouslySetInnerHTML={{ __html: style.css }}
+		/>
+	))
+
+	return {
+		...initialProps,
+		emotionStyleTags,
+	}
+}
+
+export default MyDocument
