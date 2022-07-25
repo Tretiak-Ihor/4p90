b633cbc6bca4ec392141c5fe4b4a215c0ed4fe32
Tretiak Ihor <tretiak.ihor.work@gmail.com>
2022-07-26 01:17:47 +0300

    chore: Layer0
    Remove team from Layer0 config

--- layer0.config.js
+++ layer0.config.js
@@ -3,10 +3,9 @@ const { join } = require('path')
 module.exports = {
 	connector: '@layer0/next',
 	name: '4p90.com',
-	team: 'Ihor Tretiak',
 	prerenderConcurrency: 200,
 	includeFiles: {
 		[join('.next', 'BUILD_ID')]: true,
 	},
-	routes: './layer0/routes',
+	routes: './layer0/routes.js',
 }
