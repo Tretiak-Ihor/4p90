cfc746bec15bf5107c3c6730b1d8cf096349e21f
Tretiak Ihor <tretiak.ihor.work@gmail.com>
2022-07-26 01:04:18 +0300

    chore: Layer0
    Update Layer0 config

--- layer0.config.js
+++ layer0.config.js
@@ -1,3 +1,12 @@
+const { join } = require('path')
+
 module.exports = {
 	connector: '@layer0/next',
+	name: '4p90.com',
+	team: 'Ihor Tretiak',
+	prerenderConcurrency: 200,
+	includeFiles: {
+		[join('.next', 'BUILD_ID')]: true,
+	},
+	routes: './layer0/routes',
 }
