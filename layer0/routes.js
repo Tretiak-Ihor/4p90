b5bd9f72e15282c13ddf3af268c59dd57febc95a
Tretiak Ihor <tretiak.ihor.work@gmail.com>
2022-07-27 20:04:20 +0300

    perf: Layer0 cache
    Add cache config

--- layer0/routes.js
+++ layer0/routes.js
@@ -2,10 +2,14 @@ const { Router } = require('@layer0/core/router')
 const { nextRoutes } = require('@layer0/next')
 
 const prerenderRequests = require('./prerenderRequests')
+const { cacheHandler, SSR, SERVICE_WORKER, STATIC_ASSETS } = require('./cache')
 
 module.exports = new Router()
 	.prerender(prerenderRequests)
-	.match('/service-worker.js', ({ serviceWorker }) => {
+	.match('/service-worker.js', ({ cache, serviceWorker }) => {
+		cache(SERVICE_WORKER)
 		return serviceWorker('.next/static/service-worker.js')
 	})
+	.match('/', cacheHandler(SSR))
+	.match('/:file*.js', cacheHandler(STATIC_ASSETS))
 	.use(nextRoutes)
