5cb3fd2f6b17e4c1db8502dd706b8c3285dfb264
Tretiak Ihor <tretiak.ihor.work@gmail.com>
2022-08-01 16:24:27 +0300

    chore: Meta element for viewport
    Add meta element for viewport

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
 					{/* PWA primary color */}
 					<meta name="theme-color" content={theme.palette.primary.main} />
 					<link rel="shortcut icon" href="/favicon/favicon.ico" />
